2e9fdada5a4a80feecf386ca2bd5ae0d034eaba9
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-28 17:45:28 +0100

    milestone 2 problema

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<!-- **Milestone 1**
-Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
-Scriviamo tutto (logica e layout) in un unico file *index.php* -->
-
 <?php
-$pswd_lgt = $_GET[strlen("pswd")] ?? "";
+$pswd_lgt = $_GET["pswd"] ?? "";
+include __DIR__ . "/partials/functions.php";
 
+if ($pswd_lgt === "") {
+    $pswd_lgt = "";
+} else {
+    $casual_pswd = randomPassword($pswd_lgt);
+}
 
 ?>
 
@@ -16,14 +18,20 @@ $pswd_lgt = $_GET[strlen("pswd")] ?? "";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- CDN BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Pass Generator PHP</title>
 </head>
 
 <body>
     <section>
         <form action="index.php" method="GET">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="pswd">
+            <label for="password"></label>
+            <p>Inserisci di quanti caratteri vuoi che sia composta la tua password</p>
+            <input type="text" name="pswd" id="password">
+            <button type="submit">Invia</button>
+            <button type="reset">Reset</button>
+            <h4><?php echo $casual_pswd; ?></h4>
         </form>
     </section>
 </body>
